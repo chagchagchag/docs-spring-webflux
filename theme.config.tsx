92ccfce8bfe5603bba18cb5299bc056f95f44de8
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-01 11:41:23 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Spring Webflux</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-spring-webflux',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-spring-webflux',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
