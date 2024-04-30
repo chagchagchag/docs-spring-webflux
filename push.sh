782c16b1bd94e27cf6464b1828aaf7862383f8e6
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-30 12:50:55 +0900

    push.sh

---
+++ push.sh
@@ -0,0 +1,11 @@
+echo "git config --global http.version HTTP/1.1"
+git config --global http.version HTTP/1.1
+echo ""
+
+echo "git push -u origin main"
+git push -u origin main
+echo ""
+
+echo "git config --global http.version HTTP/2"
+git config --global http.version HTTP/2
+echo ""
